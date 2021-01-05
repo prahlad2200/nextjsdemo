2618e9e42bbb5f2b2bd07fe23d81669df6781313
Prahlad Makwana <prahlad@jetbro.in>
2021-01-05 15:13:44 +0530

    update

--- pages/users.js
+++ pages/users.js
@@ -1,40 +1,41 @@
-import React from 'react'
-import UserTableList from './components/TableList'; 
+import React from 'react';
+import UserTableList from './components/TableList';
 
-function Users({tableHeader,tableRows}) {
-    {console.log(tableHeader)}
-    return ( 
+function Users({ tableHeader, tableRows }) {
+    {
+        console.log(tableHeader);
+    }
+    return (
         <>
-        <UserTableList tableHeader={tableHeader} tableRows = {tableRows}/>
+            <UserTableList tableHeader={tableHeader} tableRows={tableRows} />
         </>
-      )  
-}  
-export async function getServerSideProps(context) { 
-    const tableHeader = ["ID", "Name", "User Name", "Email", "Address", "Phone"];
+    );
+}
+export async function getServerSideProps(context) {
+    const tableHeader = ['ID', 'Name', 'User Name', 'Email', 'Address', 'Phone'];
     const tableRows = [];
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users`)
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
     const userList = await res.json();
     if (!userList) {
         return {
-        notFound: true,
-        }
-    }
-    else{  
+            notFound: true
+        };
+    } else {
         for (const _user of userList) {
             var tempRow = {};
             tempRow.id = _user.id;
             tempRow.name = _user.name;
             tempRow.username = _user.username;
             tempRow.email = _user.email;
-            tempRow.address = _user.address.street + ", " + _user.address.city;
+            tempRow.address = _user.address.street + ', ' + _user.address.city;
             tempRow.phone = _user.phone;
             tableRows.push(tempRow);
-        } 
+        }
     }
 
     return {
-        props: {tableHeader,tableRows}, // will be passed to the page component as props
-    }
+        props: { tableHeader, tableRows } // will be passed to the page component as props
+    };
 }
 
-  export default Users
+export default Users;
